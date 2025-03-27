98f280c2e3359a56997e40399d2e8c7d1318f692
vitinho0076.ff <vitinho0076.ff@gmail.com>
2025-03-27 02:30:50 +0000

    inicio dos ajustes para receber a porcentagem de gorjeta escolhida

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0 
 let numberOfPeople = 0
+let buttonSelected = null
 
 let billInput = document.querySelector("#bill")
 billInput.addEventListener("input", receiveBillValue)
@@ -10,7 +11,7 @@ numberOfPeopleInput.addEventListener("input", receiveNumberOfPeopleValue)
 
 function receiveBillValue(){
     bill = billInput.valueAsNumber
-    
+
     calculate()
 }
 
@@ -18,6 +19,10 @@ function receiveNumberOfPeopleValue(){
     numberOfPeople = numberOfPeopleInput.valueAsNumber
 
     calculate()
+}
+
+function receiveTipPercentageValue(value){
+    buttonSelected = document.querySelector("")
 }
 
 function calculate(){
